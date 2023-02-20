de6610ecc90e19b012165f85b99f2be410245c61
DophBone <114565174+alvaroVB334@users.noreply.github.com>
2023-02-20 13:05:51 +0100

    commit

---
+++ phpDocker/index.php
@@ -0,0 +1,10 @@
+<?php
+    $i="Numero 1";
+    echo "Prueba";
+    echo $i;
+
+    //Crear un docker file en la misma carpeta que el archivo anterior, la carpeta del copy sea /var/www/html
+    // FROM UBUNTU 
+    // RUN
+    // COPY
+    //CMD
